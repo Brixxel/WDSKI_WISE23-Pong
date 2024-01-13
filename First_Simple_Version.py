c014df700ee6074f141b4a331c94d50389e4c2f2
Felix Regler <felix.regler03@gmail.com>
2024-01-13 17:04:17 +0100

    First_Simple_Version.py

--- First_Simple_Version.py
+++ First_Simple_Version.py
@@ -18,6 +18,14 @@ screen = pygame.display.set_mode((screen_width,screen_heigth))
 pygame.display.set_caption('Pong')
 
 
+#Game Rectangel
+ball = pygame.Rect((screen_width/2) - 15, (screen_heigth/2) - 15, 30,30)
+player = pygame.Rect((screen_width - 20), (screen_heigth/2 -70), 10, 140)
+opponent = pygame.Rect(10, screen_heigth/ 2 - 70, 10, 140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
 
 
 while True:
@@ -26,6 +34,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    #Visuals
+    screen.fill(bg_color)
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, light_grey, ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2,0), (screen_width/2,screen_heigth))
+    
     
     
     #Updating the window
