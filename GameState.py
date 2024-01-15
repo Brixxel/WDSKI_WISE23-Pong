a1d99a5a3de396c55052df56d79fdb3e022cf7cf
Felix Regler <felix.regler03@gmail.com>
2024-01-16 00:03:00 +0100

    added Spiel-Modi erhöhe Geschwindigkeit nach Zeit

--- GameState.py
+++ GameState.py
@@ -5,8 +5,7 @@ import Player, Paddel, AiPlayer, Ball
 class GameState_Manager:
     
     def __init__(self, screen):
-        
-        
+    
         # !!! evtl ja nach KI anpassen
         self.player_ai = AiPlayer.AIPlayer()
         self.opponent_score = 0
@@ -23,21 +22,38 @@ class GameState_Manager:
         self.screen = screen
         self.screen_width = screen.get_width()
         self.screen_height = screen.get_height()
+        
+        self.game_timer = 0
 
+        # Design Standarts
         self.standart_font = pygame.font.Font('freesansbold.ttf', 32)
         self.accent_color = (27,35,43)
+        
+        # Optionale Spiel-Modi-Ergänzungen:
+        self.game_modus_feature_incresingSpeed = False          # Spielmodus, bei dem nach Spiel-Zeit, die Reflexion verstärkt wird
+        self.game_modus_feature_incresingReflektion = False
+       
         
     def run_game(self):
 		# Drawing the game objects
         self.paddle_group.draw(self.screen)
         self.ball_group.draw(self.screen)
+        
+        #Auführen besonderer Spiel-Modi
+        if self.game_modus_feature_incresingSpeed:
+           self.feature_incresing_Speed()
+         
 
 		# Updating the game objects
         self.paddle_group.update(self.ball_group)
         self.ball_group.update()
         self.reset_ball()
         self.draw_score()
+        
+        # Timer / run-Durchlauf um eins erhöhen
+        self.game_timer += 1
     
+    # Wenn ein Punkt erziehlt wurde: (Sowohl Überprüfung wie auch Score Erhöhung)
     def reset_ball(self):
         if self.ball_group.sprite.rect.right >= self.screen_width:
             self.paddle_player_2.player.score += 1
@@ -56,6 +72,27 @@ class GameState_Manager:
 
         self.screen.blit(player_1_score,player_score_rect)
         self.screen.blit(opponent_score,opponent_score_rect)
+        
+    
+# --------------------------------------------------------------------------------------- #
+# Spiel-Modi Funktioen
+# --------------------------------------------------------------------------------------- #
+    
+    # Spiel-Modus: je länger gespielt wird, umso schneller wird der Ball
+    def feature_incresing_Speed(self):
+        if self.game_modus_feature_incresingSpeed:
+            if self.game_timer % 1000 == 0:
+                self.ball.speed_x = self.ball.speed_x * 1.1
+                self.ball.speed_y = self.ball.speed_y * 1.1
+                # !!!!! Print Statement entfehrnen
+                print(f"Erhöhe Geschwindigkeit auf: {self.ball.speed_x}")
+
+
+ 
+# --------------------------------------------------------------------------------------- #
+# Spiel - Initialisierungs / Generierungs Methoden
+# --------------------------------------------------------------------------------------- # 
+ 
  
  
 # New Game Methode, die den Speilstand des bisherigen Spiels löscht und mit den neuen Paddels beginnt
@@ -69,20 +106,24 @@ class GameState_Manager:
 
         self.ball = Ball.Ball('Ball.png', self.screen_width/2, self.screen_height/2, 4, 4, self.paddle_group, self.screen_height, self.screen_width, self.screen)
         self.ball_group.add(self.ball)
+        
+        #mit dem Erstellen eines neuen Spiels, muss der Timer zurück gesetzt werden
+        self.game_timer = 0
+        
         return "PvAi"
 
     def Start_PvP_Game(self, player_1 : Player, player_2 : Player):
-        
-        
         
         self.paddle_player_1 = Paddel.Paddel(player_1, self.screen_width - 20, self.screen_height/2, 5, self.screen_height)
         self.paddle_player_2 = Paddel.Paddel(player_2,20,self.screen_width/2, 5, self.screen_height)
 
         self.paddle_group.add(self.paddle_player_1)
         self.paddle_group.add(self.paddle_player_2)
-        
-        # ... (Alle weiteren Paddels)
 
         self.ball = Ball.Ball('Ball.png', self.screen_width/2, self.screen_height/2, 4, 4, self.paddle_group, self.screen_height, self.screen_width, self.screen)
         self.ball_group.add(self.ball)
+        
+        #mit dem Erstellen eines neuen Spiels, muss der Timer zurück gesetzt werden
+        self.game_timer = 0
+        
         return "PvP"
